79e1e3c8efdcf35eca0bb343df60bcc1fea0b9c8
Hochang <twojini98@gmail.com>
2025-04-29 03:42:22 +0900

    vite -> vue3

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './MobanogiCalc/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: './',
 })
